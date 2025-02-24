628eed3565e06a946849cca4e14e854aec71429c
andresesm <andresesm2@gmail.com>
2025-02-23 21:20:36 -0300

    r6 ss js

--- gallery-randomizer.js
+++ gallery-randomizer.js
@@ -1,10 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     const galleryContainer = document.querySelector('.gallery-images2');
     const images = [
-        './assets/insidegallery/rzgallery  (1).webp',
-        './assets/insidegallery/rzgallery  (2).webp',
-        './assets/insidegallery/rzgallery  (3).webp',
-        // ...continúa hasta la imagen 29
+        './assets/insidegallery/rzgallery(1).webp',
+        './assets/insidegallery/rzgallery(2).webp',
+        './assets/insidegallery/rzgallery(3).webp',
+        './assets/insidegallery/rzgallery(4).webp',
+        './assets/insidegallery/rzgallery(5).webp',
+        './assets/insidegallery/rzgallery(6).webp',
+        './assets/insidegallery/rzgallery(7).webp',
+        './assets/insidegallery/rzgallery(8).webp',
+        './assets/insidegallery/rzgallery(9).webp',
+        './assets/insidegallery/rzgallery(10).webp',
+        './assets/insidegallery/rzgallery(11).webp',
+        './assets/insidegallery/rzgallery(12).webp',
+        './assets/insidegallery/rzgallery(13).webp',
+        './assets/insidegallery/rzgallery(14).webp',
+        './assets/insidegallery/rzgallery(15).webp',
+        './assets/insidegallery/rzgallery(16).webp',
+        './assets/insidegallery/rzgallery(17).webp',
+        './assets/insidegallery/rzgallery(18).webp',
+        './assets/insidegallery/rzgallery(19).webp',
+        './assets/insidegallery/rzgallery(20).webp',
+        './assets/insidegallery/rzgallery(21).webp',
+        './assets/insidegallery/rzgallery(22).webp',
+        './assets/insidegallery/rzgallery(23).webp',
+        './assets/insidegallery/rzgallery(24).webp',
+        './assets/insidegallery/rzgallery(25).webp',
+        './assets/insidegallery/rzgallery(26).webp',
+        './assets/insidegallery/rzgallery(27).webp',
+        './assets/insidegallery/rzgallery(28).webp',
+        './assets/insidegallery/rzgallery(29).webp',
     ];
 
     // Función para mezclar las imágenes
