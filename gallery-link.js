1b35ebada8eb57a417475fb0d5be39e9265bf9af
andresesm <andresesm2@gmail.com>
2025-02-24 12:00:01 -0300

    rs3

---
+++ gallery-link.js
@@ -0,0 +1,6 @@
+document.getElementById('galleryClick').addEventListener('click', function() {
+    window.open('https://drive.google.com/drive/folders/1OPHCBHYtgFTd7uuETCy5FNEFxg291lKj?usp=drive_link', '_blank'); // Cambia esto por tu enlace
+  });
+  
+
+  
