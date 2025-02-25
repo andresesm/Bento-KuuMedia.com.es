d8f57ebdf42565deb4fa0783dc9f8f4c8998dc6a
andresesm <andresesm2@gmail.com>
2025-02-25 11:35:15 -0300

    sin js y con href a

--- gallery-link.js
+++ gallery-link.js
@@ -1,4 +1,4 @@
-/*  if (window.innerWidth > 768) {
+  if (window.innerWidth > 768) {
     // Solo ejecuta el JS si el ancho es mayor que 768px
     // Esto asegura que el JS solo se ejecute en pantallas grandes (desktops/tablets)
     document.addEventListener('DOMContentLoaded', function() {
@@ -7,8 +7,8 @@
           });
           
     });
-}*/
+}
 
-document.getElementById('galleryClick').addEventListener('click', function() {
+/*document.getElementById('galleryClick').addEventListener('click', function() {
     window.open('https://drive.google.com/drive/folders/1OPHCBHYtgFTd7uuETCy5FNEFxg291lKj?usp=drive_link', '_blank'); // Cambia esto por tu enlace
-});
+});*/
