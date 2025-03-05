921457d5eb7268358ff067f1fb384aadf7076489
Alejandro Sánchez <alejandroszg@gmail.com>
2025-03-05 16:32:05 -0300

    fix: arreglando la huea

--- gallery-link.js
+++ gallery-link.js
@@ -1,12 +1,16 @@
-  if (window.innerWidth > 768) {
-    // Solo ejecuta el JS si el ancho es mayor que 768px
-    // Esto asegura que el JS solo se ejecute en pantallas grandes (desktops/tablets)
-    document.addEventListener('DOMContentLoaded', function() {
-        document.getElementById('galleryClick').addEventListener('click', function() {
-            window.open('https://drive.google.com/drive/folders/1OPHCBHYtgFTd7uuETCy5FNEFxg291lKj?usp=drive_link', '_blank'); // Cambia esto por tu enlace
-          });
-          
-    });
+if (window.innerWidth > 768) {
+  // Solo ejecuta el JS si el ancho es mayor que 768px
+  // Esto asegura que el JS solo se ejecute en pantallas grandes (desktops/tablets)
+  document.addEventListener("DOMContentLoaded", function () {
+    document
+      .getElementById("galleryClick")
+      .addEventListener("click", function () {
+        window.open(
+          "https://drive.google.com/drive/folders/1OPHCBHYtgFTd7uuETCy5FNEFxg291lKj?usp=drive_link",
+          "_blank"
+        ); // Cambia esto por tu enlace
+      });
+  });
 }
 
 /*document.getElementById('galleryClick').addEventListener('click', function() {
